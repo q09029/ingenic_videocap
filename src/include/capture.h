9c004c0ed1478db3b4abef42ff180c48ad394b93
q09029 <q09029@shibaura-it.ac.jp>
2022-07-11 12:19:23 +0900

    t31_gc2053 i2c addr change

--- src/include/capture.h
+++ src/include/capture.h
@@ -32,9 +32,9 @@
 #include <alsa/asoundlib.h>
 
 
-//#define SENSOR_NAME				"jxf22"
+//#define SENSOR_NAME				"gc2053"
 #define SENSOR_CUBS_TYPE        TX_SENSOR_CONTROL_INTERFACE_I2C
-#define SENSOR_I2C_ADDR			0x40
+#define SENSOR_I2C_ADDR			0x37
 #define SENSOR_WIDTH			1920
 #define SENSOR_HEIGHT			1080
 #define CHN0_EN                 1
